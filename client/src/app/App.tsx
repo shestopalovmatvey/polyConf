1916b2dc8c765bcc056cd322e15a56d8c2b0eb69
shestopalovmatvey <shestopalov.myu@edu.spbstu.ru>
2023-11-15 01:50:20 +0300

    добавлена страница SignUpPage + стили к ней

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -3,6 +3,7 @@ import HomePage from "../pages/HomePage/HomePage"
 import ChatPage from "../pages/ChatPage/ChatPage"
 import { ConnectPage } from "../pages/ConnectPage/ConnectPage"
 import { LoginPage } from "../pages/LoginPage/LoginPage"
+import { SignUpPage } from "../pages/SignUpPage/SignUpPage"
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/join" element={<ConnectPage />}/>
         <Route path="/room/:roomId" element={<ChatPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/signUp" element={<SignUpPage />} />
       </Routes>
     </>
   )
