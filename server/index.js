8c8f64ed7cb23a84f2e9320d6affc5c96f5cdf0c
shestopalovmatvey <shestopalov.myu@edu.spbstu.ru>
2023-11-15 12:26:17 +0300

    Исправлены отступы у LoginPage и SignUpPage

--- server/index.js
+++ server/index.js
@@ -1,1 +1,12 @@
-const express = requer
+const express = require('express');
+
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.status(200).json('Сервер работает.')
+});
+
+app.listen(5000, () => {
+    console.log('Server running on port 3000');
+});
